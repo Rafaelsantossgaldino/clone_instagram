8a40b3865cf63e682bf50ffdf2098542f76faa95
Rafaelsantossgaldino <rafaelsantossgaldino2@gmail.com>
2022-09-26 22:14:58 -0300

    add migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_26_130812) do
+ActiveRecord::Schema.define(version: 2022_09_26_234717) do
 
   create_table "posts", force: :cascade do |t|
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "created_by_id", null: false
+    t.index ["created_by_id"], name: "index_posts_on_created_by_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.string "username", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
+  end
+
+  add_foreign_key "posts", "users", column: "created_by_id"
 end
