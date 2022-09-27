972c15cbd6d7ea77a77e63bd5472ed27eb22f99c
Rafaelsantossgaldino <rafaelsantossgaldino2@gmail.com>
2022-09-26 22:15:21 -0300

    add users in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root "posts#index"
-  
+
   devise_for :users, only: %i[sessions registrations passwords]
 
   resources :posts, only: %i[index show new create]
+  resources :users, only: %i[show], param: :username
 end
