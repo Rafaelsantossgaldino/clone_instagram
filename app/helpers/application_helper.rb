533db755a61b3a96d0890eefcd97c17427f1ff23
Rafaelsantossgaldino <rafaelsantossgaldino2@gmail.com>
2022-09-29 14:23:11 -0300

    add metho in helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def embedded_svg(path)
     File.open("app/assets/images/#{path}", "rb") { |file| raw(file.read) }
   end
+
+  def user_avatar(user)
+    avatar = user.avatar.variant(combine_options: {resize_to_fill: '100x100^', extent: '100x100'}) if user.avatar.attached?
+    avatar = "default-avatar.jpg"
+    image_tag avatar, class: "photo"
+  end
 end
